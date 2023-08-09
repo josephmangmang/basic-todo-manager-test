9620f38da2388c5198593036175a598ffe1659da
josephmangmang <joseph@blendit.eu>
2023-08-09 14:25:11 +0800

    initial commit

---
+++ lib/app/app.dart
@@ -0,0 +1,32 @@
+import 'package:taskmanager/ui/bottom_sheets/notice/notice_sheet.dart';
+import 'package:taskmanager/ui/dialogs/info_alert/info_alert_dialog.dart';
+import 'package:taskmanager/ui/views/home/home_view.dart';
+import 'package:taskmanager/ui/views/startup/startup_view.dart';
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:taskmanager/ui/views/task_list/task_list_view.dart';
+// @stacked-import
+
+@StackedApp(
+  routes: [
+    MaterialRoute(page: HomeView),
+    MaterialRoute(page: StartupView),
+    MaterialRoute(page: TaskListView),
+// @stacked-route
+  ],
+  dependencies: [
+    LazySingleton(classType: BottomSheetService),
+    LazySingleton(classType: DialogService),
+    LazySingleton(classType: NavigationService),
+    // @stacked-service
+  ],
+  bottomsheets: [
+    StackedBottomsheet(classType: NoticeSheet),
+    // @stacked-bottom-sheet
+  ],
+  dialogs: [
+    StackedDialog(classType: InfoAlertDialog),
+    // @stacked-dialog
+  ],
+)
+class App {}
